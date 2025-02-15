b1da2d5ef82a69544ac2f2eda2a2536035c06e60
kanyo <kanyo@Abdou.Home>
2025-02-15 03:22:16 +0100

    fix

--- app/api/gameweek/start/route.js
+++ app/api/gameweek/start/route.js
@@ -1,5 +1,6 @@
 import { startGameweek } from "@/controllers/startGameWeek";
 
 export async function POST(req, res) {
-  return startGameweek(req, res);
+  startGameweek(req, res)
+  return Response(200, { message: 'Gameweek started' });
 }
